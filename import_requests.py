6f07785a663e4845a9398150d833fc51543ab276
leejwjaden <165357675+leejwjaden@users.noreply.github.com>
2024-04-27 17:13:39 -0400

    Publish to Supabase added

--- import_requests.py
+++ import_requests.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from supabase_py import create_client, Client
 # # WeatherDetailsListItem--label--2ZacS This is labels for general data
 # # WeatherDetailsListItem--wxData--kK35q This is general data for day
 
@@ -20,4 +20,15 @@ for location in locationCodes:
 for location, temps in highLowSet:
     print(f'{location}: {temps}')
 
-print("test")
+url = 'https://rwuiwmxmiopyovvrgpkm.supabase.co'
+key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ3dWl3bXhtaW9weW92dnJncGttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NjY1MDQsImV4cCI6MjAyODQ0MjUwNH0.LJ46wnMzEGJMWB46VvSPuGE7It5CCApXTB6dbiCyFI0'
+supabase: Client = create_client(url, key)
+
+
+for location, temps in highLowSet:
+    data = {
+        'location': location,
+        'temps': temps
+    }
+    response = supabase.table('weather_data').insert(data).execute()
+
